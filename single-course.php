bfa44b04f585b2e99857618b907eafabf36ba62e
zeeshaneagle09 <zeeshaneagle09@gmail.com>
2026-09-05 00:56:44 +0500

    Wire premium continuation into single course

--- single-course.php
+++ single-course.php
@@ -13,45 +13,20 @@ get_header();
 ?>
 
 <main class="single-page">
-
 	<div class="container">
-
 		<?php while ( have_posts() ) : the_post(); ?>
-
 			<div class="single-layout">
-
 				<article class="single-main">
-
-					<?php
-					get_template_part(
-						'template-parts/single/single-header'
-					);
-
-					get_template_part(
-						'template-parts/single/single-content'
-					);
-					?>
-
+					<?php get_template_part( 'template-parts/single/single-header' ); ?>
+					<?php get_template_part( 'template-parts/single/single-content' ); ?>
+					<?php get_template_part( 'template-parts/single/author-card' ); ?>
+					<?php get_template_part( 'template-parts/single/post-continuation' ); ?>
 				</article>
-
-				<?php
-				get_template_part(
-					'template-parts/single/single-sidebar'
-				);
-				?>
-
+				<?php get_template_part( 'template-parts/single/single-sidebar' ); ?>
 			</div>
-
-			<?php
-			get_template_part(
-				'template-parts/single/related-content'
-			);
-			?>
-
+			<?php get_template_part( 'template-parts/single/related-content' ); ?>
 		<?php endwhile; ?>
-
 	</div>
-
 </main>
 
 <?php get_footer(); ?>
