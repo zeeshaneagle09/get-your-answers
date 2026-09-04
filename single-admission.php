8e17a2562181fbfda412b433ae326a8c56189f53
zeeshaneagle09 <zeeshaneagle09@gmail.com>
2026-09-05 00:49:36 +0500

    Add author section to single admission

--- single-admission.php
+++ single-admission.php
@@ -13,45 +13,23 @@ get_header();
 ?>
 
 <main class="single-page">
-
 	<div class="container">
-
 		<?php while ( have_posts() ) : the_post(); ?>
-
 			<div class="single-layout">
-
 				<article class="single-main">
-
 					<?php
-					get_template_part(
-						'template-parts/single/single-header'
-					);
-
-					get_template_part(
-						'template-parts/single/single-content'
-					);
+					get_template_part( 'template-parts/single/single-header' );
+					get_template_part( 'template-parts/single/single-content' );
+					get_template_part( 'template-parts/single/author-card' );
 					?>
-
 				</article>
 
-				<?php
-				get_template_part(
-					'template-parts/single/single-sidebar'
-				);
-				?>
-
+				<?php get_template_part( 'template-parts/single/single-sidebar' ); ?>
 			</div>
 
-			<?php
-			get_template_part(
-				'template-parts/single/related-content'
-			);
-			?>
-
+			<?php get_template_part( 'template-parts/single/related-content' ); ?>
 		<?php endwhile; ?>
-
 	</div>
-
 </main>
 
 <?php get_footer(); ?>
