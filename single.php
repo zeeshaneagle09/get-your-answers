3c0ac2dbb3c6621d1aded0e42d2e0c5a05153bb3
zeeshaneagle09 <zeeshaneagle09@gmail.com>
2026-09-05 00:57:06 +0500

    Wire premium continuation into default single

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Single post template.
+ * Default single post template.
  *
  * @package Get_Your_Answers_Daily
  */
@@ -12,30 +12,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 ?>
 
-<div class="single-page">
-
+<main class="single-page">
 	<div class="container">
-
 		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'template-parts/single/single-header' ); ?>
-
-			<div class="single-article-layout">
-
-				<main class="single-article-main">
-
+			<div class="single-layout">
+				<article class="single-main">
+					<?php get_template_part( 'template-parts/single/single-header' ); ?>
 					<?php get_template_part( 'template-parts/single/single-content' ); ?>
-
-				</main>
-
+					<?php get_template_part( 'template-parts/single/author-card' ); ?>
+					<?php get_template_part( 'template-parts/single/post-continuation' ); ?>
+				</article>
 				<?php get_template_part( 'template-parts/single/single-sidebar' ); ?>
-
 			</div>
-
+			<?php get_template_part( 'template-parts/single/related-content' ); ?>
 		<?php endwhile; ?>
-
 	</div>
-
-</div>
+</main>
 
 <?php get_footer(); ?>
